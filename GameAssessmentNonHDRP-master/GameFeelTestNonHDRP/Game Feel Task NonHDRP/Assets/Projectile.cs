6ae1b1bb4f5bbd9d648488637d02f5cab28fd66a
soumya mondal <iamsammyyo@gmail.com>
2021-11-03 13:18:30 +0530

    First Commit

---
+++ GameAssessmentNonHDRP-master/GameFeelTestNonHDRP/Game Feel Task NonHDRP/Assets/Projectile.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    public float speed = 10;
+    public Rigidbody _RB;
+    public float waitTime = 2f;
+    private void Awake()
+    {
+        _RB = GetComponent<Rigidbody>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        waitTime -= Time.deltaTime;
+        if (waitTime <= 0)
+        {
+            _RB.velocity = new Vector3(0, 0, -speed);
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(gameObject);
+    }
+}
---
+++ GameAssessmentNonHDRP-master/README.md
@@ -0,0 +1,7 @@
+# Game Developer Assessment
+
+This is a 24 hour timed assignment which will assess your skill as a game programmer. When you have finished your exam, please push the results to the repository which will be given to you. If you have any issues during the course of the assessment, please contact your hiring liason at Hopscotch Health
+
+You can feel free to use any resource including online libraries and tutorials. You are not permitted to consult with friends for the project
+
+[Detailed assessment instructions here](https://docs.google.com/document/d/1tMqqku5wOYLQ3Es-JAFIMSDDns-QxTBM5_Au68DEmI8)
